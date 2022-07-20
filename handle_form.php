3d859fbe8029cf8c2505c6eda9d23bd2676c9eb4
samuelcarmack <36247178+samuelcarmack@users.noreply.github.com>
2022-07-20 14:36:02 -0400

    Add files via upload

---
+++ handle_form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<?php
+$gender = $_POST["gender"];
+$name = $_POST["name"];
+$favorite_movie = $_POST["persons_favorite_movie"];
+
+echo "My name is $name";
+echo "My gender is $gender";
+echo "My favortie movie is $favorite_movie";
+?>
+
+
+
+
+  </body>
+</html>
